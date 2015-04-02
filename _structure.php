5456c4ebc39e85d662a523fc7677dff99977b3ab
mwyatt <martin.wyatt@gmail.com>
2015-04-02 17:26:01 +0100

    begin sass and template ground work

---
+++ _structure.php
@@ -0,0 +1,62 @@
+<?php
+
+
+/**
+ * create template for each title here which will be included
+ * fill those templates with relevant examples
+ * array
+ */
+return [
+	'scaffolding' => [
+		'html5-doctype',
+		'box-sizing',
+		'normalize',
+		'bourbon'
+	],
+	'layout' => [
+		'grid',
+		'floats',
+		'clearfix',
+		'text-align'
+	],
+	'typography' => [
+		'heading',
+		'body',
+		'lead',
+		'inline',
+		'blockquotes',
+		'lists'
+	],
+	'buttons' => [
+		'default-buttons',
+		'primary',
+		'secondary',
+		'tertiary',
+		'outline',
+		'disabled',
+		'block'
+	],
+	'form' => [
+		'input-text',
+		'input-number',
+		'select',
+		'textarea',
+		'label',
+		'radio',
+		'checkbox',
+		'note',
+		'submit (button)',
+		'sizing' => [
+			'small',
+			'big',
+			'block'
+		]
+	],
+	'alert' => [
+		'normal',
+		'success',
+		'failure',
+		'dismiss'
+	],
+	'blank-slate' => []
+];
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+$structure = include '_structure.php';
+
+foreach ($structure as $firstLevelName => $firstLevel) {
+	
+}
