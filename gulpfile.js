65990e7151547c43b8b47b28c93af60c759b72cc
Martin Wyatt <martin.wyatt@gmail.com>
2016-02-05 08:33:32 +0000

    codex deps

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,7 @@
-var pkg = require('./package.json');
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
-var rename = require('gulp-rename');
-var imagemin = require('gulp-imagemin');
-var pngquant = require('imagemin-pngquant');
-var csscomb = require('gulp-csscomb');
-var postcss = require('gulp-postcss');
-var jscs = require('gulp-jscs');
-var uglify = require('gulp-uglify');
 var browserify = require('browserify');
-var csswring = require('csswring');
 var source = require('vinyl-source-stream');
-var eventStream = require('event-stream');
-var glob = require('glob');
 var autoprefixer = require('autoprefixer');
 var colorFunction = require('postcss-color-function');
 var hexrgba = require('postcss-hexrgba');
@@ -25,79 +14,16 @@ var processes = [
   autoprefixer({browsers: ['last 1 version']})
 ];
 
-gulp.task('copy', function() {
-  gulp.src('jsVendor/**')
-    .pipe(gulp.dest('asset/vendor'));
-  return gulp.src('media/**')
-    .pipe(gulp.dest('asset'));
-});
-
 gulp.task('css', function () {
-  return gulp.src('**.bundle.css')
+  return gulp.src('common.css')
     .pipe(postcss(processes))
-    .pipe(rename(function (pathObject) {
-      pathObject.basename = pathObject.basename.replace('.bundle', '');
-    }))
-    .pipe(gulp.dest('asset'));
-});
-
-gulp.task('css-tidy', function() {
-  return gulp.src('**.css')
-    .pipe(csscomb())
-    .pipe(gulp.dest('./'));
-});
-
-gulp.task('css-min', function () {
-  processes.push(csswring);
-  return gulp.src('**.bundle.css')
-    .pipe(postcss(processes))
-    .pipe(rename(function (pathObject) {
-      pathObject.basename = pathObject.basename.replace('.bundle', '');
-    }))
     .pipe(gulp.dest('asset'));
 });
 
 gulp.task('js', function(done) {
-  glob('**.bundle.js', function(err, files) {
-    if (err) {
-      done(err);
-    };
-    var tasks = files.map(function(entry) {
-      return browserify({
-        entries: [entry],
-        paths: [
-          '.'
-        ]
-      })
-      .bundle()
-      .pipe(source(entry.replace('js/', '').replace('.bundle', '')))
-      .pipe(gulp.dest('asset'));
-    });
-
-    // create a merged stream
-    eventStream.merge(tasks).on('end', done);
-  });
-});
-
-gulp.task('js-tidy', function () {
-  return gulp.src('js/common.bundle.js')
-    .pipe(jscs({
-      'preset': 'google'
-    }));
-});
-
-gulp.task('js-min', function() {
-  gulp.src('asset/**.js')
-    .pipe(uglify())
+  return browserify()
+    .add('common.js')
+    .bundle()
+    .pipe(source('common.js'))
     .pipe(gulp.dest('asset'));
-});
-
-gulp.task('image-min', function () {
-  return gulp.src('media/**')
-    .pipe(imagemin({
-      progressive: true,
-      svgoPlugins: [{removeViewBox: false}],
-      use: [pngquant()]
-    }))
-    .pipe(gulp.dest('media'));
 });
