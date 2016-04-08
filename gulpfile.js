b864f8d58f0b2c318a44db1dfc5d723d5e47b513
Martin <martin.wyatt@gmail.com>
2016-04-08 11:10:45 +0100

    gulp moved files back into one, packages as devdeps

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,17 @@
 require('es6-promise').polyfill(); // required to fix postcss-import?
 
-var settings = require('./gulp/settings');
+var settings = {
+  isLocal: true,
+  assetDest: 'asset/',
+  watch: {
+    css: 'css/**/*.css',
+    js: 'js/**/*.js'
+  },
+  css: 'css/',
+  js: 'js/',
+  media: ['media/']
+};
+
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var gulpConcat = require('gulp-concat');
@@ -11,13 +22,32 @@ var uglify = require('gulp-uglify');
 var cssmin = require('gulp-cssmin');
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
+var autoprefixer = require('autoprefixer');
+var postcss = require('gulp-postcss');
+var postcssImport = require('postcss-import');
+var postcssCsscomb = require('postcss-csscomb');
+var postcssCombOptions = require('./.csscomb.json');
+var postcssColorFunction = require('postcss-color-function');
+var postcssHexrgba = require('postcss-hexrgba');
+var postcssConditionals = require('postcss-conditionals');
+var postcssCustomProperties = require('postcss-custom-properties');
+var postcssProcesses = [
+  postcssImport,
+  postcssCustomProperties(),
+  postcssConditionals,
+  postcssHexrgba(),
+  postcssColorFunction(),
+  autoprefixer({browsers: ['last 1 version']})
+];
+var browserify = require('browserify');
+var buffer = require('gulp-buffer');
 
 gulp.task('watch', watch);
 gulp.task('min', min);
-gulp.task('css', require('./gulp/css'));
+gulp.task('css', css);
 gulp.task('cssMin', cssMin);
 gulp.task('cssTidy', cssTidy);
-gulp.task('js', require('./gulp/js'));
+gulp.task('js', js);
 gulp.task('jsLib', jsLib);
 gulp.task('jsMin', jsMin);
 gulp.task('jsTidy', jsTidy);
@@ -35,6 +65,27 @@ function watch() {
   gulp.watch(settings.watch.css, ['css']);
   gulp.watch(settings.watch.js, ['js']);
 }
+
+function js() {
+  return gulp.src(settings.js + '**/*.bundle.js', {read: false})
+    .pipe(tap(function(file) {
+      file.contents = browserify(file.path, {debug: settings.isLocal})
+        .transform('browserify-shim', {global: true})
+        .bundle();
+      gutil.log('build ' + file.path);
+    }))
+    .pipe(buffer())
+    .pipe(gulp.dest(settings.assetDest));
+};
+
+function css() {
+  return gulp.src(settings.css + '**/*.bundle.css')
+    .pipe(postcss(postcssProcesses))
+    .pipe(tap(function(file) {
+      gutil.log('build ' + file.path);
+    }))
+    .pipe(gulp.dest(settings.assetDest));
+};
 
 function cssMin() {
   return gulp.src(settings.assetDest + '**/*.css')
