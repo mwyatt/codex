e69cec9b29541f58c725406dcc230f689e15d02c
Martin <martin.wyatt@gmail.com>
2016-04-13 08:14:53 +0100

    strip away scripts

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
 <?php
 
+echo '<pre>';	
+print_r('variable');
+echo '</pre>';
+exit;
 session_start();
 $pathBase = (string) (__DIR__ . '/');
 include $pathBase . 'vendor/autoload.php';
 $request = new \Mwyatt\Core\Request;
 
-if ($request->getServer('HTTP_HOST') === '192.168.1.6') {
-	ini_set('display_startup_errors',1);
-	ini_set('display_errors',1);
-	error_reporting(-1);
-}
+ini_set('display_startup_errors',1);
+ini_set('display_errors',1);
+error_reporting(-1);
 
 $url = new \Mwyatt\Core\Url($request->getServer('HTTP_HOST'), $request->getServer('REQUEST_URI'), 'codex/');
 $view = new \Mwyatt\Core\View;
@@ -21,7 +23,6 @@ $view->data->offsetSet('url', $url);
 $routes = array_merge(
     include $view->getPathBase('routes.php')
 );
-
 $router = new \Mwyatt\Core\Router(new \Pux\Mux);
 
 $router->appendMuxRoutes($routes);
