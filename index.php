fb0fdd6155eadec1386124c9dfc3562bfde59e3b
Martin <martin.wyatt@gmail.com>
2016-05-11 17:29:03 +0100

    progress dist page

--- index.php
+++ index.php
@@ -3,26 +3,42 @@
 <head>
 	<meta charset="utf-8">
 	<title>Codex</title>
-	<link href="//fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800" rel="stylesheet" type="text/css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <link href="//fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800" rel="stylesheet" type="text/css">
 	<link href="asset/common.bundle.css" media="screen, projection, print" rel="stylesheet" type="text/css" />
 </head>
 <body>
-	<input type="hidden" value="<?php echo 'parsed' ?>">
 	<div class="body-inner-container clearfix">
 		<div class="site-heading-container">
-			<h1 class="site-heading">Codex <span class="npm-version">2.*.*</span></h1>
+			<h1 class="site-heading">Codex <span class="npm-version">2</span></h1>
 			<div class="site-slogan">A refreshing start for your css.</div>
+			<div class="site-heading-menu">
+			    <div class="site-heading-menu-item"><a class="site-heading-menu-link button" href="https://www.npmjs.com/package/mwyatt-codex" target="_blank">NPM</a></div>
+			    <div class="site-heading-menu-item"><a class="site-heading-menu-link button" href="https://github.com/mwyatt/codex" target="_blank">Github</a></div>
+			</div>
 		</div>
-		<div class="site-heading-menu">
-		    <div class="site-heading-menu-item"><a class="site-heading-menu-link button" href="https://www.npmjs.com/package/mwyatt-codex" target="_blank">NPM</a></div>
-		    <div class="site-heading-menu-item"><a class="site-heading-menu-link button" href="https://github.com/mwyatt/codex" target="_blank">Github</a></div>
+		<div class="topics-container">
+			<div class="topic-container">
+				<h2 class="topic-heading">Form</h2>
+				<p class="p">About the form area.</p>
+				<h3 class="topic-heading-secondary">Radio</h3>
+				<div class="topic-html">
+					<label for="example-radio" class="form-label block">Example Label</label>
+					<input id="example-radio" class="form-radio" name="example" type="radio">
+					<label for="example-radio-2" class="form-label block">Example Label</label>
+					<input id="example-radio-2" class="form-radio" name="example" type="radio">
+				</div>
+			</div>
+			<div class="topic-container">
+				<h2 class="topic-heading">Button</h2>
+				<div class="topic-html">
+					<label for="example-radio" class="form-label block">Example Label</label>
+					<input id="example-radio" class="form-radio" name="example" type="radio">
+					<label for="example-radio-2" class="form-label block">Example Label</label>
+					<input id="example-radio-2" class="form-radio" name="example" type="radio">
+				</div>
+			</div>
 		</div>
 	</div>
-
-	<!-- <script>var phpUrl = {"base":"192.168.2.231\/codex\/","path":"","routes":{"home":""},"protocol":"https:\/\/"}</script> -->
-
-	<!-- <script src="asset/lib.js?1"></script> -->
-	<!-- <script src="asset/common.bundle.js?1"></script> -->
 </body>
 </html>
