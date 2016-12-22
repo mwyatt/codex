7a346a4a0fce25dc292c96a344a6a14e492d95ee
Martin <martin.wyatt@gmail.com>
2016-12-22 17:22:55 +0000

    10 most common html tags

--- template/home.php
+++ template/home.php
@@ -19,9 +19,18 @@
 		</div>
 		<div class="topics-container">
 			<div class="topic-container">
+				<h2 class="topic-heading">Reset</h2>
+				<div class="typography">
+					<ul>
+						<li><a href="https://necolas.github.io/normalize.css/" target="_blank">normalize.css</a></li>
+						<li>All margins nulled.</li>
+					</ul>
+				</div>
+			</div>
+			<div class="topic-container">
 				<h2 class="topic-heading">Typography</h2>
+				<p class="topic-description p">Use the class 'typography' to wrap typographic elements. This will style them automatically.</p>
 				<div class="topic-container-secondary">
-					<h3 class="topic-heading-secondary">Heading</h3>
 					<div class="topic-html typography">
 						<h1>Heading 1</h1>
 						<h2>Heading 2</h2>
@@ -29,30 +38,32 @@
 						<h4>Heading 4</h4>
 						<h5>Heading 5</h5>
 						<h6>Heading 6</h6>
-					</div>
-				</div>
-				<div class="topic-container-secondary">
-					<h3 class="topic-heading-secondary">Paragraph</h3>
-					<div class="topic-html typography">
-						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-					</div>
-				</div>
-				<div class="topic-container-secondary">
-					<h3 class="topic-heading-secondary">List</h3>
-					<div class="topic-html typography">
+						<h2>Paragraph</h2>
+						<p>Paragraph - Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+						<h2>Italic</h2>
+						<i>Italic</i>
+						<h2>Bold</h2>
+						<b>Bold</b>
+						<a href="#">Anchor</a>
 						<ul>
-							<li>List item 1</li>
-							<li>List item 2</li>
-							<li>List item 3</li>
+							<li>Unordered List item 1</li>
+							<li>Unordered List item 2</li>
+							<li>Unordered List item 3</li>
 						</ul>
 						<ol>
-							<li>List item 1</li>
-							<li>List item 2</li>
-							<li>List item 3</li>
+							<li>Ordered List item 1</li>
+							<li>Ordered List item 2</li>
+							<li>Ordered List item 3</li>
 						</ol>
+						<blockquote>Blockquote - Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</blockquote>
 					</div>
 				</div>
 			</div>
+
+			<hr>
+			<img src="http://placehold.it/50x50" alt="Placeholder">
+			<div>Division</div>
+
 			<div class="topic-container">
 				<h2 class="topic-heading">Form</h2>
 				<div class="topic-container-secondary">
