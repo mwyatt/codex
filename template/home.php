eff29c3f2e21e549ef4a907df971f9f8fe5df00c
Martin <martin.wyatt@gmail.com>
2016-12-23 16:59:58 +0000

    tweaks to home

--- template/home.php
+++ template/home.php
@@ -23,7 +23,6 @@
 				<div class="typography">
 					<ul>
 						<li><a href="https://necolas.github.io/normalize.css/" target="_blank">normalize.css</a></li>
-						<li>All margins nulled.</li>
 					</ul>
 				</div>
 			</div>
@@ -33,11 +32,17 @@
 				<div class="topic-container-secondary">
 					<div class="topic-html typography">
 						<h1>Heading 1</h1>
+						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
 						<h2>Heading 2</h2>
+						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
 						<h3>Heading 3</h3>
+						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
 						<h4>Heading 4</h4>
+						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
 						<h5>Heading 5</h5>
+						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
 						<h6>Heading 6</h6>
+						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
 						<h2>Paragraph</h2>
 						<p>Paragraph - Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
 						<h2>Italic</h2>
@@ -59,11 +64,6 @@
 					</div>
 				</div>
 			</div>
-
-			<hr>
-			<img src="http://placehold.it/50x50" alt="Placeholder">
-			<div>Division</div>
-
 			<div class="topic-container">
 				<h2 class="topic-heading">Form</h2>
 				<div class="topic-container-secondary">
