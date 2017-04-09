6593592eb50e4012beaaac9eb273f9294177d7ba
Martin <martin.wyatt@gmail.com>
2017-04-09 22:08:22 +0100

    attempt at bms nav recreation

--- template/home.php
+++ template/home.php
@@ -28,8 +28,21 @@
 			</div>
 			<div class="topic-container">
 				<h2 class="topic-heading">BMS Menu</h2>
-				<div class="topic-html">
-					hi
+				<div class="bms-nav">
+
+<?php foreach ([1, 2, 3] as $key): ?>
+
+					<div class="bms-nav-item">
+						<a href="#" class="bm-nav-item-link">CRM</a>
+						<div class="bms-nav-item-children">
+							<div class="bms-nav-item-child">
+								<a href="#" class="bms-nav-item-child-link">Dashboard</a>
+							</div>
+						</div>
+					</div>
+
+<?php endforeach ?>
+
 				</div>
 			</div>
 			<div class="topic-container">
